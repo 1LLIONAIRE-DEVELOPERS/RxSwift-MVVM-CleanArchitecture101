ad1f9b38624353abb2acd7b498982af52af9d3bc
July911 <min95911@nate.com>
2022-05-13 18:30:03 +0900

    feat: Add Repository Type

--- final/RxSwift-MVVM-CleanArchitecture101/RxSwift-MVVM-CleanArchitecture101/Data/List+DTO.swift
+++ final/RxSwift-MVVM-CleanArchitecture101/RxSwift-MVVM-CleanArchitecture101/Data/List+DTO.swift
@@ -36,16 +36,6 @@ struct Item: Codable {
     }
 }
 
-struct Owner: Codable {
-    let login: String
-    let avatarURL: String
-
-    enum CodingKeys: String, CodingKey {
-        case login
-        case avatarURL = "avatar_url"
-    }
-}
-
 extension List {
     func toDomain() -> RepoItem {
         
