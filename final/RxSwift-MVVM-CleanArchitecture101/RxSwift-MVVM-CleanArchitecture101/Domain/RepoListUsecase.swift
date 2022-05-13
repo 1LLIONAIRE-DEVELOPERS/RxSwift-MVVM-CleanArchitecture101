1b080c2364c7348cd364098904897ca79f0f16a0
jsim27 <simjh27@gmail.com>
2022-05-13 17:01:35 +0900

    feat: Implement RepoListUsecase

---
+++ final/RxSwift-MVVM-CleanArchitecture101/RxSwift-MVVM-CleanArchitecture101/Domain/RepoListUsecase.swift
@@ -0,0 +1,35 @@
+//
+//  RepoListUsecase.swift
+//  RxSwift-MVVM-CleanArchitecture101
+//
+//  Created by Jae-hoon Sim on 2022/05/13.
+//
+
+import Foundation
+import RxSwift
+
+class RepoListUsecase {
+
+    let repoItemRepository: RepoItemRepository
+
+    init(repoItemRepository: RepoItemRepository) {
+        self.repoItemRepository = repoItemRepository
+    }
+
+    func fetch(
+        query: String,
+        sortBy: RepoItemQuerySortType?,
+        isDescending: Bool,
+        itemsPerPage: Int,
+        pageNumber: Int
+    ) -> Observable<[RepoItem]> {
+        return self.repoItemRepository.fetch(
+            query: query,
+            sortBy: sortBy,
+            isDescending: isDescending,
+            itemsPerPage: itemsPerPage,
+            pageNumber: pageNumber
+        )
+    }
+
+}
