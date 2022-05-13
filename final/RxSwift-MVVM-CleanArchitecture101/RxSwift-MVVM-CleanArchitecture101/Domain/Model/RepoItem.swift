dcb4acc9925f5c781e352144bc28a842170b5ff2
jsim27 <simjh27@gmail.com>
2022-05-13 16:35:10 +0900

    feat: Create RepoItem Model Type

---
+++ final/RxSwift-MVVM-CleanArchitecture101/RxSwift-MVVM-CleanArchitecture101/Domain/Model/RepoItem.swift
@@ -0,0 +1,17 @@
+//
+//  RepoItem.swift
+//  RxSwift-MVVM-CleanArchitecture101
+//
+//  Created by Jae-hoon Sim on 2022/05/13.
+//
+
+import Foundation
+
+struct RepoItem {
+    let name: String
+    let description: String
+    let ownerName: String
+    let ownerAvatarURL: String
+    let starCount: Int
+    let language: String
+}
