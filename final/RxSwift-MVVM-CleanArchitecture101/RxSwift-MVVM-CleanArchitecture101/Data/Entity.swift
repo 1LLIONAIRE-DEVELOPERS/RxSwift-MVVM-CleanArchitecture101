95db841e49e435a1bdf3b84efe8a4e18687ee2d4
July911 <min95911@nate.com>
2022-05-13 16:59:34 +0900

    feat: APIRequest Type add & create Entity

---
+++ final/RxSwift-MVVM-CleanArchitecture101/RxSwift-MVVM-CleanArchitecture101/Data/Entity.swift
@@ -0,0 +1,47 @@
+//
+//  Entity.swift
+//  RxSwift-MVVM-CleanArchitecture101
+//
+//  Created by 조영민 on 2022/05/13.
+//
+
+import Foundation
+
+struct Welcome: Codable {
+    let totalCount: Int
+    let incompleteResults: Bool
+    let items: [Item]
+
+    enum CodingKeys: String, CodingKey {
+        case totalCount = "total_count"
+        case incompleteResults = "incomplete_results"
+        case items
+    }
+}
+
+struct Item: Codable {
+    let name, fullName: String
+    let owner: Owner
+    let itemDescription: String
+    let stargazersCount: Int
+    let language: String?
+
+    enum CodingKeys: String, CodingKey {
+        case name
+        case fullName = "full_name"
+        case owner
+        case itemDescription = "description"
+        case stargazersCount = "stargazers_count"
+        case language
+    }
+}
+
+struct Owner: Codable {
+    let login: String
+    let avatarURL: String
+
+    enum CodingKeys: String, CodingKey {
+        case login
+        case avatarURL = "avatar_url"
+    }
+}
---
+++ final/RxSwift-MVVM-CleanArchitecture101/RxSwift-MVVM-CleanArchitecture101/NetWork/APIEndPoints.swift
@@ -0,0 +1,18 @@
+//
+//  APIEndPoints.swift
+//  RxSwift-MVVM-CleanArchitecture101
+//
+//  Created by 조영민 on 2022/05/13.
+//
+
+import Foundation
+
+final class APIHeader {
+    
+}
+
+final class QueryParameters {
+    
+}
+
+
