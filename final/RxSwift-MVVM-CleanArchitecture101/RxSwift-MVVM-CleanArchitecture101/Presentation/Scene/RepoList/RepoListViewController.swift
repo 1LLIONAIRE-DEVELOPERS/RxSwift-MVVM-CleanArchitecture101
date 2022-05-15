f78c236b44d609dc86e9b3effcd46773cadac0f7
July <min95911@nate.com>
2022-05-16 01:04:35 +0900

    feat: Create ViewModel Binding Method & Subsribe ViewModel Stream

--- final/RxSwift-MVVM-CleanArchitecture101/RxSwift-MVVM-CleanArchitecture101/Presentation/Scene/RepoList/RepoListViewController.swift
+++ final/RxSwift-MVVM-CleanArchitecture101/RxSwift-MVVM-CleanArchitecture101/Presentation/Scene/RepoList/RepoListViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import RxSwift
+import RxRelay
 
 class RepoListViewController: UIViewController {
     var viewModel: RepoListViewModel?
@@ -46,6 +47,23 @@ class RepoListViewController: UIViewController {
     }
 
     private func binding() {
-        //TODO: Binding 
+        let searchQuery = BehaviorRelay<String>(value: "")
+        self.searchController.searchBar.rx.text.orEmpty
+            .subscribe(onNext: { str in
+            searchQuery.accept(str)
+            }).disposed(by: self.disposeBag)
+        
+        let input = RepoListViewModel.Input(
+            viewWillAppear: self.rx.methodInvoked(#selector(UIViewController.viewWillAppear(_:))).map { _ in },
+            searchQuery: searchQuery,
+            cellDidTap: self.tableView.rx.modelSelected(RepoItem.self).asObservable()
+        )
+        let output = self.viewModel?.transform(input: input)
+        
+        output?.repoItems.asDriver(onErrorJustReturn: [])
+            .drive(self.tableView.rx.items(cellIdentifier: String(describing: TableViewCell.self), cellType: TableViewCell.self)) {
+                index, data, item in
+                //cell.configure(item)
+            }.disposed(by: self.disposeBag)
     }
 }
